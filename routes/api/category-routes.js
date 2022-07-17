27d386bcc2a2427c729ec193dfd4801cc502599c
Zach Burkhart <zachburkhart216@gmail.com>
2022-07-17 17:22:17 -0400

    added category-routes data

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Model } = require('sequelize/types');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -6,6 +7,18 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }]
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
